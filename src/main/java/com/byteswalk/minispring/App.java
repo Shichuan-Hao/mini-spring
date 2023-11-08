da0fa03a0d67efcbdf8c5b34c4a9cafa0a76d712
hsc <haoshichuan@foxmail.com>
2023-11-09 01:00:45 +0800

    初始化项目

---
+++ src/main/java/com/byteswalk/minispring/App.java
@@ -0,0 +1,13 @@
+package com.byteswalk.minispring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
